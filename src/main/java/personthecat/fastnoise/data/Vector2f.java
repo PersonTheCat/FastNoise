91c65da1127d61ded6c215e3b8ec34b5e06c1a3b
PersonTheCat <professionallyperson@gmail.com>
2021-08-01 19:24:41 -0500

    Delete unused classes

--- src/main/java/personthecat/fastnoise/data/Vector2f.java
+++
@@ -1,8 +0,0 @@
-package personthecat.fastnoise.data;
-
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public class Vector2f {
-    public float x, y;
-}
--- src/main/java/personthecat/fastnoise/data/Vector3f.java
+++
@@ -1,8 +0,0 @@
-package personthecat.fastnoise.data;
-
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public class Vector3f {
-    public float x, y, z;
-}

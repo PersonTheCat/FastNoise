f68615402aa2fedfed315df12dd8c0ddcbcad31f
PersonTheCat <professionallyperson@gmail.com>
2022-01-28 16:08:31 -0600

    Update default value

--- src/main/java/personthecat/fastnoise/data/NoiseDescriptor.java
+++ src/main/java/personthecat/fastnoise/data/NoiseDescriptor.java
@@ -21,7 +21,7 @@ public class NoiseDescriptor {
     private CellularDistanceType distance = CellularDistanceType.EUCLIDEAN;
     private CellularReturnType cellularReturn = CellularReturnType.CELL_VALUE;
     private NoiseDescriptor[] noiseLookup = {};
-    private MultiType multi = MultiType.MAX;
+    private MultiType multi = MultiType.SUM;
     private int seed = 1337;
     private float frequencyX = 0.01F;
     private float frequencyY = 0.01F;
